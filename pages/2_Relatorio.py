5e9c4e9f6a6c1c3e83c673d797d604b5e61b4681
MarceloLSantos <marcelo.lsantos@gmail.com>
2024-05-18 17:18:07 -0300

    teste

--- pages/2_Relatorio.py
+++ pages/2_Relatorio.py
@@ -1,5 +1,7 @@
-# import streamlit as st
+import streamlit as st
 # import pandas as pd
+
+st.title("CERTIFAST - RELATÓRIOS") 
 
 # st.set_page_config(page_title="CERTIFAST - RELATÓRIOS", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 

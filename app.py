073b8e91fb6321985ae7641d7300fafe222af4c6
Marcelo L Santos <marcelo.lsantos@gmail.com>
2024-11-15 13:19:52 -0300

    Pagina unica

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-# st.set_page_config(page_title="CERTIFAST - RELATÓRIOS", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="CERTIFAST - RELATÓRIOS", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 # Pegar dados de Parceiros
 colunas_parceiros = ['Nome Vendedor',

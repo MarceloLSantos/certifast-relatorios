5ad26d272c415cbfa037e2bb20cee75dd02b02b3
MarceloLSantos <marcelo.lsantos@gmail.com>
2025-02-23 15:42:50 -0300

    Excluir arquivo update

--- app.py
+++ app.py
@@ -183,13 +183,11 @@ if st.session_state.logged_in == True:
             def excluir_arquivo(arquivo):
                 os.remove(f'./dados/{arquivo}')
                 st.success(f'Arquivo {arquivo} excluído com sucesso')
-                
+
             # Listar todos os arquivos excel da pasta dados e permitir o usuário escolher qual deseja excluir
             arquivos = os.listdir('./dados')
             arquivos = [arquivo for arquivo in arquivos if arquivo.endswith('.xlsx')]
             arquivos = [arquivo for arquivo in arquivos if 'R-' in arquivo or 'V-' in arquivo]
-            arquivos = [arquivo for arquivo in arquivos if 'Parceiros.xlsx' not in arquivo and 'Repasses.xlsx' not in arquivo]
-            arquivos = [arquivo for arquivo in arquivos if 'Revenda' not in arquivo and 'Validacoes' not in arquivo]
 
             st.markdown('<p class="sub-header color-blue">EXCLUIR ARQUIVOS</p>', unsafe_allow_html=True)
             arquivo_selecionado = st.selectbox('Selecione o arquivo para excluir', arquivos, key='arquivo_selecionado')
